2038c106a271f79868bf59503cf42d09261064dc
Algimantas Januška <falja005@edu.xamk.fi>
2024-02-04 02:17:46 +0200

    first commit

---
+++ src/routes/index.jsx
@@ -0,0 +1,13 @@
+export default function Index() {
+    return (
+      <p id="zero-state">
+        This is God speaking stop fooling around:).
+        <br />
+        Check out{" "}
+        <a href="https://www.godweb.org/">
+          Study my son:)
+        </a>
+        .
+      </p>
+    );
+  }
